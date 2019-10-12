fa4eaf344ea35bb061f6632cec9cc807079c496c
maoxiaoning <1627881936@qq.com>
2019-10-12 09:04:02 +0800

    注册

--- vue/src/http.js
+++ vue/src/http.js
@@ -15,11 +15,17 @@ axios.interceptors.request.use(function (config) {
  
 // 响应拦截器
 axios.interceptors.response.use(function (response) {
+  console.log('122');
+  
     // Do something with response data
     return response;
   }, function (error) {
+    console.log(error);
+    
       //当token过期时，获取错误信息，并清除本地存储的过期token
       if(error.response.status == 401){
+        console.log(response);
+        
         localStorage.removeItem('mytoken')
         router.push('/login')   //跳转到登录组件
       }
