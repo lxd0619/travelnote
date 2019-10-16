0566eafa57840948d119333a11c4fb9554f507e2
szalexping <pxcai12345@Hotmail.com>
2019-10-16 19:31:43 +0800

    19:31

--- vue/src/views/View.vue
+++ vue/src/views/View.vue
@@ -2,35 +2,71 @@
   <div>
     <div id="head"></div>
     <div class="contain">
-      <div id="circleContent" class="carousel slide carousel-fade" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#circleContent" data-slide-to="0" class="active"></li>
-          <li data-target="#circleContent" data-slide-to="1"></li>
-          <li data-target="#circleContent" data-slide-to="2"></li>
-          <li data-target="#circleContent" data-slide-to="3"></li>
-          <li data-target="#circleContent" data-slide-to="4"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="../assets/view/景色背景图片.jpg" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="../assets/view/景色背景图片2.jpg" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="../assets/view/景色图片3.jpg" />
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#circleContent" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#circleContent" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
+        <div class="total">
+        <section id="timeline">
+          <div class="totalcontainer">
+            <div class="mine-item">
+              <div class="figcaption">
+                <div class="bg bg1"></div>
+                <div class="name">
+                  <p>记忆</p>
+                </div>
+                <div class="content">
+                  <p>品尝到一份别致的美食，会让你久久不能忘记。一份精致的美食，如一份可爱的礼物一样，当你拿到手的时候，除了珍惜，就是赞叹。</p>
+                </div>
+              </div>
+            </div>
 
+            <div class="mine-item">
+              <div class="figcaption">
+                <div class="bg bg2"></div>
+                <div class="name">
+                  <p>味道</p>
+                </div>
+                <div class="content">
+                
+                  <p>果汁新鲜，酸甜，当你倦怠不堪时，刺激你的味蕾和感觉神经，让你新鲜难以忘怀，在心中留下了特别的味道</p>
+                </div>
+              </div>
+            </div>
+            <div class="mine-item">
+              <div class="figcaption">
+                <div class="bg bg3"></div>
+                <div class="name">
+                  <p>传承</p>
+                </div>
+                <div class="content">
+                  <p>寻常的衣食住行中，传统日复一日地延续</p>
+                </div>
+              </div>
+            </div>
+            <div class="mine-item">
+              <div class="figcaption">
+                <div class="bg bg4"></div>
+                <div class="name">
+                  <p>相逢</p>
+                </div>
+                <div class="content">
+                  <p>
+                    人们用五味杂陈形容人生，因为懂得，味道是每个心中固守的乡愁
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="mine-item">
+              <div class="figcaption">
+                <div class="bg bg5"></div>
+                <div class="name">
+                  <p>流年</p>
+                </div>
+                <div class="content">
+                  <p>当你的掌纹静静地滑过这些旧物的骨架，时光酿造的陈酒便会悄然擦过舌尖的味蕾</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
       <div class="container-fluid waveline">
         <img src="../assets/view/line2.png" width="100%" />
       </div>
@@ -42,7 +78,7 @@
           <img src="../assets/bg.jpg" width="260px" height="240px" />
           <div class="intro_box">
             <h3>{{article.title}}</h3>
-            <p>{{article.fsInfo}}</p>
+            <p>{{article.ssInfo}}</p>
             <span>
               <img src="../assets/food/收藏_line.png" width="17px" height="17px" />
               点赞({{article.fsLikeNum}})
@@ -467,5 +503,152 @@ h1 {
   padding-left: 50px;
   width: 260px;
   overflow: hidden;
+}
+/* 背景样式 */
+.total {
+  margin-top: 40px;
+}
+
+.totalcontainer {
+  display: flex;
+  padding: 0;
+}
+
+.container:hover .mine-item {
+  width: 23.5%;
+}
+
+.mine-item:hover {
+  width: 32.5% !important;
+  
+}
+
+.mine-item {
+  transform: translate3d(0, 0, 0);
+  position: relative;
+  width: 25%;
+  height: 100%;
+  min-height: 600px;
+  max-height: 800px;
+  color: white;
+  overflow: hidden;
+  transition: width 0.5s ease;
+}
+
+.mine-item:hover .figcaption:after {
+  opacity: 0;
+}
+
+.figcaption:after {
+  transform: translate3d(0, 0, 0);
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgb(233, 220, 200);
+  opacity: 0.8;
+  /*不透明度。0透明1不透明*/
+  /* transition: opacity 0.5s ease; */
+}
+
+.mine-item:hover .figcaption:before {
+  transform: translateY(0);
+  opacity: 1;
+  transition: all 0.5s ease;
+}
+
+.figcaption:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(116, 116, 116, 0) 0%,
+    rgba(0, 0, 0, 0) 90%
+  );
+  z-index: 1;
+  opacity: 0;
+  transform: translate3d(0, 0, 0) translateY(50%);
+}
+
+.mine-item:hover .content {
+  opacity: 1;
+  transform: translateY(0px);
+  transition: all 0.75s ease 0.5s;
+}
+
+.content {
+  width: 100%;
+  transform: translate3d(0, 0, 0) translateY(250px);
+  position: absolute;
+  top: 55%;
+  z-index: 1;
+  text-align: center;
+  padding: 0 1.618em;
+  opacity: 0;
+}
+
+.name {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  /*相对于自身移动。就相当于把原来左上角的重心移到了正中间*/
+  z-index: 2;
+  border-top: 1px solid white;
+  border-bottom: 1px solid white;
+}
+
+.name p {
+  text-transform: uppercase;
+  font-size: 1.728rem;
+  line-height: 1;
+  margin: 0;
+  padding: 3px;
+}
+
+.mine-item:hover .bg {
+  filter: grayscale(0);
+}
+
+.bg{
+  transform: translate3d(0, 0, 0);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-size: cover;
+  background-position: center center;
+  transition: filter 0.5s ease;
+  filter: graycale(50%);
+}
+ .bg1{
+   background-image:url(../assets/food/2.jpg)}
+ .bg2{
+   background-image:url(../assets/food/3.jpg)}
+  .bg3{
+   background-image:url(../assets/food/timg.jpg)}
+ .bg4{
+   background-image:url(../assets/food/food11.jpg)}
+  .bg5{
+   background-image:url(../assets/food/food6.jpg)}
+h2{
+  text-align: center;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.tab_title ul {
+  float: left;
+  margin: 10px 0 0 50px;
+}
+
+.container #content a {
+  height: 100%;
+  display: block;
 }
 </style>
