ce54380f7fa2622e8c2068983a82ea142d44b63e
lxd0619 <1228372022@qq.com>
2019-11-11 23:56:26 +0800

    123

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,9 +1,11 @@
 var dbconfig = {
-    host: '127.0.0.1',
-    // host: '192.168.2.6',
-    port: 3306,
+    // host: '127.0.0.1',
+    // port: 3306,
     database: 'travelnote',
     user: 'root',
-    password: '123456'
+    // password: '123456',
+    port: 10203,
+    host: 'cdb-6iz73m26.bj.tencentcdb.com',
+    password: 'mmsnsr@123'
 }
 module.exports = dbconfig
